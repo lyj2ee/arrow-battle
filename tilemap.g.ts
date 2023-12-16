4b41cacb685ee2f1a94eada947ef93f43d8f45de
lyj2ee <134721892+lyj2ee@users.noreply.github.com>
2023-12-16 17:31:35 +0800

    MakeCode 项目的初始文件

---
+++ tilemap.g.ts
@@ -0,0 +1,14 @@
+// 自动生成的代码。请勿编辑。
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency18 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency18":return transparency18;
+        }
+        return null;
+    })
+
+}
+// 自动生成的代码。请勿编辑。
